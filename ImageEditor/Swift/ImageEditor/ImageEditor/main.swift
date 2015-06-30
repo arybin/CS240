87bcd3fa40782eba5404063c95936581c2cc8f85
Andrei <andrew.rybin@gmail.com>
2015-06-29 21:50:17 -0400

    starting the project in swift, probably won't be doing any in CPP, but we will see

---
+++ ImageEditor/Swift/ImageEditor/ImageEditor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ImageEditor
+//
+//  Created by Andrei Rybin on 6/29/15.
+//  Copyright (c) 2015 Andrei Rybin. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
