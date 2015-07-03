b3ffeb7606cb5cdc4f826733dd9a38e37b71c096
Andrei <andrew.rybin@gmail.com>
2015-07-03 16:20:21 -0400

    started working on the next project

---
+++ ImageEditor/Swift/ImageEditor/ImageEditor/ImageProcessor.swift
@@ -0,0 +1,32 @@
+//
+//  ImageProcessor.swift
+//  ImageEditor
+//
+//  Created by Andrei Rybin on 7/2/15.
+//  Copyright (c) 2015 Andrei Rybin. All rights reserved.
+//
+
+import Foundation
+
+class ImageProccesor {
+    
+    private var path:String
+    
+    init (path:String) {
+        self.path = path
+    }
+    
+    //read from the file or stream
+    func readStream() {
+        
+    }
+    
+    //proccess into a data structure
+    func processStream() {
+        
+    }
+    
+    private func applyAction() {
+        
+    }
+}
